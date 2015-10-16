f789f666bfecf93d2ed5ce1cacdb4fddbd6ba9b1
Michael Shick <m@shick.us>
2015-10-15 17:07:59 -0700

    working

--- src/knockout-inview.js
+++ src/knockout-inview.js
@@ -1,17 +1,22 @@
 
 const listeners = [];
 
+const isOverlapping = function (x1, x2, y1, y2) {
+  return x1 <= y2 && y1 <= x2;
+};
+
 const addListener = function (element, value) {
 
   const listener = function () {
 
     const rect = element.getBoundingClientRect();
+    const isInview = isOverlapping(rect.top, rect.bottom, 0, window.outerHeight);
 
-    if (rect.bottom <= 0 && value()) {
+    if (!isInview && value()) {
       value(false);
     }
 
-    if (rect.bottom >= 0 && !value()) {
+    if (isInview && !value()) {
       value(true);
     }
   };
